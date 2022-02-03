e2d528641bc78d3486f74f534b798c204ba3a1ee
Hotakubo <91017792+Hotakubo@users.noreply.github.com>
2022-02-03 23:43:28 +0900

    Update Labels.tsx

--- components/Labels.tsx
+++ components/Labels.tsx
@@ -2,11 +2,12 @@ import { useState } from 'react';
 import useSWR from 'swr';
 
 export default function Labels() {
-  const { data } = useSWR('/api/labels/read', async (url: string): Promise<string[]> => {
+  const { data, error } = useSWR('/api/labels/read', async (url: string): Promise<string[]> => {
     const response = await fetch(url);
 
     return response.json();
   })
+  if (error) return <li></li>
   if (!data) return <li></li>
 
   const renderLabels = data.map(label => <li key={label}>{label}</li>)
