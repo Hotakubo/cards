9668a971b713ddc9426383e372572f461b6a9a3e
Hotakubo <91017792+Hotakubo@users.noreply.github.com>
2022-05-13 00:05:56 +0900

    Update Icons.tsx

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { MdAccountBox } from "react-icons/md";
+import { MdAccountBox, MdPhoto } from "react-icons/md";
 
 const read = (icon: number) => {
   switch (icon) {
     case 23:
       return <MdAccountBox size={30}></MdAccountBox>
+
+    case 88:
+      return <MdPhoto size={30}></MdPhoto>
 
     default:
       return <div>{icon}</div>
