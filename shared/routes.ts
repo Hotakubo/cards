40819bbf382b4b5e4d52ea9dc9a15b9b8f8b1b46
Hotakubo <91017792+Hotakubo@users.noreply.github.com>
2022-05-30 08:35:31 +0900

    Update routes.ts

--- shared/routes.ts
+++ shared/routes.ts
@@ -8,7 +8,7 @@ type TypeRoute = {
 
 const ORIGIN = 'http://localhost'
 
-export const URLPath = (url: string | undefined) => {
+const URLPath = (url: string | undefined) => {
   if (!url) return ''
 
   const { pathname } = new URL(ORIGIN.concat(url))
