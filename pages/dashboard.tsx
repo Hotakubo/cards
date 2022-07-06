33cebc66994efc144ae50232f359c3b257885865
Hotakubo <91017792+Hotakubo@users.noreply.github.com>
2022-07-07 01:21:45 +0900

    Update dashboard.tsx

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,32 +1,15 @@
 import { useState } from 'react';
 import type { NextPage } from 'next';
-import type { TypeLabel } from '@/database/labels';
-import useSWR from 'swr';
 import Labels from '@/components/Labels';
 import Cards from '@/components/Cards';
 
-const LabelsRead = () => {
-  const { data, error } = useSWR('/api/labels', async (url: string): Promise<TypeLabel[]> => {
-    const response = await fetch(url);
-
-    return response.json();
-  });
-
-  return { labels: data, error };
-};
-
 const Dashboard: NextPage = () => {
-  const [label, setLabel] = useState(0);
-  const { labels } = LabelsRead();
+  const [enabledLabel, setEnabledLabel] = useState(0);
 
   return (
     <div className="flex">
-      <Labels
-        labels={labels ? labels : []}
-        label={label}
-        onLabel={label => setLabel(label)}
-      ></Labels>
-      <Cards label={label}></Cards>
+      <Labels enabledLabel={enabledLabel} onClick={label => setEnabledLabel(label)}></Labels>
+      <Cards label={enabledLabel}></Cards>
     </div>
   );
 };
