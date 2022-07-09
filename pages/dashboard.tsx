871bd9759900a909951db1df99cf4dc0939563f7
Hotakubo <91017792+Hotakubo@users.noreply.github.com>
2022-07-10 02:00:51 +0900

    Update dashboard.tsx

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import type { NextPage } from 'next';
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 import Labels from '@/components/Labels';
 import Cards from '@/components/Cards';
 
@@ -7,10 +9,12 @@ const Dashboard: NextPage = () => {
   const [enabledLabel, setEnabledLabel] = useState(0);
 
   return (
-    <div className="flex">
-      <Labels enabledLabel={enabledLabel} onClick={label => setEnabledLabel(label)}></Labels>
-      <Cards label={enabledLabel}></Cards>
-    </div>
+    <DndProvider backend={HTML5Backend}>
+      <div className="flex">
+        <Labels enabledLabel={enabledLabel} onClick={label => setEnabledLabel(label)}></Labels>
+        <Cards label={enabledLabel}></Cards>
+      </div>
+    </DndProvider>
   );
 };
 
